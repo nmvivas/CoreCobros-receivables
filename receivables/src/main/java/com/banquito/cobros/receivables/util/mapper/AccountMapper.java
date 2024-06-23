c2af266778d802102f2384e1c15056d2f098cd4f
Sebastian Salazar <sasalazar10@espe.edu.ec>
2024-06-22 20:25:35 -0500

    correccion mapper accounts

--- receivables/src/main/java/com/banquito/cobros/receivables/util/mapper/AccountMapper.java
+++ receivables/src/main/java/com/banquito/cobros/receivables/util/mapper/AccountMapper.java
@@ -1,6 +1,7 @@
 package com.banquito.cobros.receivables.util.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.ReportingPolicy;
 import com.banquito.cobros.receivables.dto.AccountDTO;
@@ -8,7 +9,10 @@ import com.banquito.cobros.receivables.model.Account;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface AccountMapper {
+
+    @Mapping(source = "company.id", target = "companyId")
     AccountDTO toDTO(Account account);
 
+    @Mapping(source = "companyId", target = "company.id")
     Account toPersistence(AccountDTO dto);
 }
