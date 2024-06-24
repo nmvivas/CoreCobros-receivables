7037ff74638cb5222670479ed1efc35fb8e0f0f3
Charles Zambrano <charleszambranoespe@outlook.com>
2024-06-24 11:52:11 -0500

    [UPDATE] Order

--- receivables/src/main/java/com/banquito/cobros/receivables/model/Order.java
+++ receivables/src/main/java/com/banquito/cobros/receivables/model/Order.java
@@ -1,23 +1,15 @@
 package com.banquito.cobros.receivables.model;
 
-import java.io.Serializable;
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.List;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
 
 @NoArgsConstructor
 @Getter
